652e77d83d452b910308a7d4f7fac654b35086e5
kamila0801 <kami7686@easv365.dk>
2022-04-28 20:00:41 +0200

    tailwind installed

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'primary-orange': '#E29219',
+        'background-grey': '#E5E5E5',
+        'primary-grey': '#838383'
+      },
+    },
+  },
+  plugins: [],
+}
