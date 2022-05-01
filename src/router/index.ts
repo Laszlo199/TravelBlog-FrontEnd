3547e7229e561c77d02650006b31d7675c801c71
soma199 <70133057+Laszlo199@users.noreply.github.com>
2022-05-01 18:14:11 +0200

    add login and register view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/search',
-      name: 'search',
-      component: () => import('../views/SearchView.vue')
-    }
-  ]
-})
+      path: "/search",
+      name: "search",
+      component: () => import("../views/SearchView.vue"),
+    },
+    {
+      path: "/login",
+      name: "login",
+      component: () => import("../views/LoginView.vue"),
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: () => import("../views/RegisterView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
