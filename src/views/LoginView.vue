3e9d7cfe0d6571a13917b795ca10812c8b24c63e
soma199 <70133057+Laszlo199@users.noreply.github.com>
2022-05-09 16:10:01 +0200

    change colors

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -41,7 +41,7 @@
         <div class="flex items-center justify-between mb-5 pt-6">
           <button
             @click="loginUser"
-            class="w-full inline-flex items-center justify-center px-4 py-2 bg-orange-500 hover:bg-orange-600 text-white font-light py-2 px-6 rounded focus:ring focus:ring-offset-amber-400 disabled:opacity-25 transition"
+            class="w-full inline-flex items-center justify-center px-4 py-2 bg-primary-orange hover:bg-primary-grey text-white font-light py-2 px-6 rounded focus:ring focus:ring-offset-amber-400 disabled:opacity-25 transition"
             type="button"
           >
             Sing In
